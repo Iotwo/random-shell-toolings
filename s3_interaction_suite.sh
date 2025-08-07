df23275f4b570fff2f243479c400a8c62fb08a3f
Wundarshular <cattugla@yandex.ru>
2025-08-07 18:06:09 +0700

    Update s3_interaction_suite.sh
    fixes

--- s3_interaction_suite.sh
+++ s3_interaction_suite.sh
@@ -365,10 +365,10 @@ curl_put_data_to_s3() {
 }
 
 wget_get_data_from_s3() {
-	#wget --verbose --server-response --header "Date: ${dt_val}" --header 'Content-Type: application/octet-stream' --header "Authorization: AWS ${a_k}:${signature}" "https://${host}/${bucket}/${object}"
+    return 0; #wget --verbose --server-response --header "Date: ${dt_val}" --header 'Content-Type: application/octet-stream' --header "Authorization: AWS ${a_k}:${signature}" "https://${host}/${bucket}/${object}"
 }
 
-wget_put_data_to_s3() {}
+wget_put_data_to_s3() {return 0;}
 
 perform_access_checks() {
 	############################################################
